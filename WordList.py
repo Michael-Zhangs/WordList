aad0bc903ca2577b7cc7a31d9f28b2f20340a4af
Michael Zhang <Michael_Zhangs@163.com>
2020-08-21 13:47:57 +0800

    added the structure

--- WordList.py
+++ WordList.py
@@ -1,1 +1,33 @@
-print ("Hello")
+#import json
+
+wordlist={}
+word={
+	"English":"",
+	"Chinese":"",
+	"times":"",
+	"correct":""
+}
+
+def get_list():
+	pass
+
+def edit_list():
+	pass
+
+def save_data():
+	pass
+
+def exercise():
+	pass
+
+def quit(mode):
+	pass
+
+def main_menu():
+	print("1.Get List")
+	print("2.Edit List")
+	print("3.Save Data")
+	print("4.Exercise")
+	print("5.Quit")
+
+main_menu()
