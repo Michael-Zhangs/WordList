61a118d2031a6a09e2b4e4d68087b3b415d7a4cb
Michael Zhang <Michael_Zhangs@163.com>
2020-08-21 19:27:50 +0800

    added edit_list

--- WordList.py
+++ WordList.py
@@ -1,5 +1,6 @@
 import json
 import os
+import math
 
 filename = "wordlist.json"
 
@@ -13,37 +14,65 @@ def get_list():
 		wordlist = []
 	while True:
 		word={
-			"English":"",
-			"Chinese":"",
-			"times":0,
-			"correct":0,
+			"En":"", #English
+			"Tr":"", #Translation
+			"ti":0,  #times
+			"cr":0,  #correct
 		}
 		tmp = input("word:")
 		if tmp == "q":
 			break
 		flag=False
 		for single_word in wordlist:
-			if single_word["English"] == tmp:
+			if single_word["En"] == tmp:
 				print("The word has already in the list!")
 				input()
 				flag=True
 				break
 		if flag:
 			continue
-		word["English"]=tmp
-		word["Chinese"]=input("Translation:")
+		word["En"]=tmp
+		word["Tr"]=input("Translation:")
 		wordlist.append(word)
 	with open(filename, 'w') as f_obj:
 		json.dump(wordlist, f_obj)
 		
 
 def edit_list():
-	print("edit list")
-	input()
+	try:
+		with open(filename) as f_obj:
+			wordlist = json.load(f_obj)
+	except FileNotFoundError:
+		print("No list is recorded")
+		input()
+		return
+	page = 0
+	set_default = 10
+	page_all = math.ceil(len(wordlist) / set_default)
+	while True:
+		os.system("clear")
+		if page*set_default+10 > len(wordlist):
+			line_tmp = len(wordlist) - page*set_default
+		else:
+			line_tmp = set_default
+		for line in range(0,line_tmp):
+			print(wordlist[page*set_default+line]["En"]+"\t"+
+				wordlist[page*set_default+line]["Tr"]+"\t"+
+				"times: "+str(wordlist[page*set_default+line]["ti"])+"\t"+
+				"correct: "+str(wordlist[page*set_default+line]["cr"]))
+		for line in range(0,10-line_tmp):
+			print()
+		print("\n\t\t"+"Page: "+str(page))
+		cmd = input()
+		if cmd == "n":
+			if page<page_all-1:
+				page=page+1
+		if cmd == "l":
+			if page>0:
+				page=page-1
+		if cmd == "q":
+			return
 
-def save_data():
-	print("save data")
-	input()
 
 def exercise():
 	print("exercise")
@@ -60,17 +89,15 @@ def default():
 def main_menu():
 	print("1.Get List")
 	print("2.Edit List")
-	print("3.Save Data")
-	print("4.Exercise")
-	print("5.Quit")
+	print("3.Exercise")
+	print("4.Quit")
 	choice = input()
 	choice = int(choice)
 	switch={	
 		1:get_list,
 		2:edit_list,
-		3:save_data,
-		4:exercise,
-		5:quit,
+		3:exercise,
+		4:quit,
 	}
 	os.system("clear")
 	switch.get(choice, default)()
